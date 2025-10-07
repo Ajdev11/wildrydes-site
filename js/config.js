a944474e885f508013bf72bb092719413c786e45
Moses Ogunsemore  Olaseni <mosesogun11@gmail.com>
2025-10-07 21:27:50 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // 
+        userPoolId: 'us-east-1_3GwMiBScS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6089l07jr1pojls7ile89rmav3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
